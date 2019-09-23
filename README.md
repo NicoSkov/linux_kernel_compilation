5c5c55586c50c4c0b28564cc4c57359ddc8ac3b5
Kaisen <kevinchevreuillinux@gmail.com>
2019-09-23 10:29:06 +0200

    Linux Kernel 5.3 compilation

---
+++ README.md
@@ -0,0 +1,5 @@
+# linux_kernel_compilation
+
+Recompiling the Linux kernel with your own configuration
+
+Recompiling the Linux kernel with your own configuration This script permit compile custom Linux kernel automatic for Debian and Ubuntu. Tested in Debian 9 and 10 and Ubuntu 19.04. This script is compatible with Linux kernel 3.x and 4.x. Changes wget link and names folders and xz/tar archives for use this script for olds Linux kernels. This script can possibility compatible Linux kernels futures versions. Changes wget link and names folders and xz/tar archives for use this script for futures Linux kernels.
---
+++ linux_kernel_compilation.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Check root execution verification
+if [[ "$EUID" -ne 0 ]]; then
+	echo -e "Run this script as root"
+	exit 1
+fi
+#Update repositories
+apt-get update
+#Upgrade packages
+apt-get -y full-upgrade
+#Dependancies install for compile kernel sources (dependancies compatibles for kernel 4.x)
+apt-get install debconf-utils dpkg-dev debhelper build-essential libncurses5-dev libelf-dev liblz4-tool bc libssl-dev xz-utils ncurses-dev git initramfs-tools dpkg-dev bin86 libglib2.0-dev libgtk2.0-dev libglade2-dev libqt4-dev pkg-config bison flex po-debconf xmlto gettext wget -y
+#Download kernel-package packet for Debian distributions
+wget http://ftp.fr.debian.org/debian/pool/main/k/kernel-package/kernel-package_13.018+nmu1~bpo9+1_all.deb
+#Install deb packet after download 
+dpkg -i kernel-package_13.018+nmu1~bpo9+1_all.deb
+#Remove kernel-package packet after install
+rm kernel-package_13.018+nmu1~bpo9+1_all.deb
+#Move to /usr/src directory for work
+cd /usr/src
+#Download kernel 5.3 on official source
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.tar.xz
+#Extract tar.xz archive
+tar xvf linux-5.3.tar.xz
+#Remove tar archive after extract 
+rm linux-5.3.tar.xz
+#Move to folder linux-5.3 obtain for extract tar archive
+cd linux-5.3/
+#Elements a compile custom kernel 
+make menuconfig
+#Build and compile custom kernel and create entries for utilisation and deb packages creates 
+#-mykernel can be replace by your choice name of your custom kernel
+#-j options can be modified by number of cores of your CPU example : -j 8 = CPU octo-core, permit compile faster
+#-j options to be modified by number of CPU's in your PC. 
+#Use command : grep -c '^processor' /proc/cpuinfo for obtain number of CPU's
+make-kpkg --append-to-version=-kernelcustom --revision=1.0 --initrd --us --uc kernel_image kernel_headers -j 4
+#Move parent directory
+cd ..
+#Install kernel custom with all .deb packages generates by make-kpkg
+dpkg -i *.deb
